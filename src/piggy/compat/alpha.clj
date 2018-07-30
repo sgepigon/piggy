91438f2f8e0be0584388037ee63045ff77414ff2
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-30 11:16:10 -0400

    [WIP] Add example specs

--- src/piggy/compat/alpha.clj
+++ src/piggy/compat/alpha.clj
@@ -3,3 +3,21 @@
             [clojure.spec.gen.alpha :as sgen]
             [clojure.spec.test.alpha :as stest]
             [expound.alpha :as expound]))
+
+
+(comment
+  "Example specs"
+
+  (s/def ::int int?)
+  (s/def ::number number?)
+
+  (s/fdef plus
+    :args (s/cat :x ::int :y ::int)
+    :ret ::int)
+
+  (s/def ::old (s/fspec :args (s/cat :x ::int :y ::int), :ret ::int))
+
+  (s/def ::new (s/fspec :args (s/cat :x ::int :y ::int), :ret ::int))
+
+  (defn plus [x y] (+ x y))
+  )
