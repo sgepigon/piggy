003202868e6186792845720ecc191ba5f2affb3e
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-30 11:01:58 -0400

    Init lein skeleton and README
    Add org-mode README.

---
+++ src/piggy/compat/alpha.clj
@@ -0,0 +1,1 @@
+(ns piggy.compat.alpha)
---
+++ test/piggy/compat/alpha_test.clj
@@ -0,0 +1,7 @@
+(ns piggy.compat.alpha-test
+  (:require [clojure.test :refer :all]
+            [piggy.compat.alpha :as compat]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
