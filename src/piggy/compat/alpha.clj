7e93a8842e9405835da5001949dd1c366f87c290
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-30 22:57:56 -0400

    Update exercise-args fspec and docstring

--- src/piggy/compat/alpha.clj
+++ src/piggy/compat/alpha.clj
@@ -13,13 +13,17 @@
                     :regex s/regex?))
 
 (s/fdef exercise-args
-  :args (s/cat :old ::args :new ::args)
-  :ret (s/every any? :count 3))
+  :args (s/alt :default (s/cat :old ::args, :new ::args)
+               :n-samples (s/cat :old ::args, :new ::args, :n nat-int?)
+               :overrides (s/cat :old ::args, :new ::args,
+                                 :n nat-int?, :overrides (s/nilable map?)))
+  :ret (s/every (s/cat :val any?, :old-conformed-val any?, :new-conformed-val any?)))
 (defn exercise-args
-  "Generates a number (default 10) of values compatible with `old` and
-  conforms the `old` and `new` specs over them, returning a sequence
-  of [val old-conformed-val new-conformed-val] tuples. For values that don't conform to `new`, the new-conformed-val will be `::s/invalid`. Optionally takes
-  a generator overrides map as per gen for `old`."
+  "Generates a number (default 10) of values compatible with `old` and conforms
+  the `old` and `new` specs over them, returning a sequence of
+  [val old-conformed-val new-conformed-val] tuples. For values that don't
+  conform to `new`, new-conformed-val will be ::s/invalid. Optionally takes a
+  generator overrides map as per gen for `old`."
   ([old new] (exercise-args old new 10))
   ([old new n] (exercise-args old new n nil))
   ([old new n overrides]
