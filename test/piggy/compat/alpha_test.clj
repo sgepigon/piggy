e27a9ff67d881265bab7dd96fa122dec86672a3b
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-31 01:38:09 -0400

    Add test for compat/exercise-args

--- test/piggy/compat/alpha_test.clj
+++ test/piggy/compat/alpha_test.clj
@@ -1,7 +1,27 @@
 (ns piggy.compat.alpha-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as sgen]
+            [clojure.spec.test.alpha :as stest]
+            [clojure.test :refer :all]
             [piggy.compat.alpha :as compat]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Example specs
+
+(s/def ::int int?)
+(s/def ::number number?)
+(s/def ::old (s/fspec :args (s/cat :x ::int :y ::int), :ret ::int))
+(s/def ::new (s/fspec :args (s/cat :x ::number :y ::number), :ret ::number))
+
+(s/fdef plus
+  :args (s/cat :x ::int :y ::int)
+  :ret ::int)
+(defn- plus [x y] (+ x y))
+
+;; testing
+
+(deftest consistency-test
+  (testing "Test conforming on the same spec gives the same answer."
+    (let [same? (fn [[_ old new]] (= old new))]
+      (is (apply = true (map same? (compat/exercise-args `plus `plus 1000))))
+      (is (apply = true (map same? (compat/exercise-args ::old `plus 1000))))
+      (is (apply = true (map same? (compat/exercise-args `plus ::old 1000)))))))
