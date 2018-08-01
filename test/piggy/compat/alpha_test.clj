a9c3f5c4b3990e2c53eec98b393f5ba235548c5b
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-08-01 14:03:08 -0400

    Fix ->args-gen generator, remove with-examples
    I actually don't need the wrapper around s/with-gen and s/gen.

--- test/piggy/compat/alpha_test.clj
+++ test/piggy/compat/alpha_test.clj
@@ -5,14 +5,6 @@
             [clojure.test :refer :all]
             [piggy.compat.alpha :as compat]))
 
-(s/fdef with-examples
-  :args (s/cat :spec s/spec? :examples set?)
-  :ret any?)
-(defn- with-examples
-  "Takes a spec and a set of examples and returns a version of that spec that uses
-  that generator of `examples`."
-  [spec examples]
-  (s/with-gen spec #(s/gen examples)))
 
 ;; Example specs
 
@@ -26,11 +18,13 @@
   :ret ::int)
 (defn- plus [x y] (+ x y))
 
-(s/def ::->args-gen
-  (s/or :symbol (with-examples (s/and qualified-symbol? ::compat/sym-or-kw->args) #{`plus `compat/valid})
-        :keyword (with-examples (s/and qualified-keyword? ::compat/sym-or-kw->args) #{::old ::new})
-        :fspec (with-examples (s/and s/spec? ::compat/fspec->args) #{(s/fspec :args (s/* some?) :ret any?)})
-        :regex (with-examples s/regex? #{(s/* any?)})))
+(defn- ->args-gen
+  "Returns a generator for `::compat/->args`"
+  []
+  (s/gen (s/or :symbol #{`plus `compat/valid}
+               :keyword #{::old ::new}
+               :fspec #{(s/fspec :args (s/* some?) :ret any?)}
+               :regex #{(s/* any?)})))
 
 ;; testing
 
