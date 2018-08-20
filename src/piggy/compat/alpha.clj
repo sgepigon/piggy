517f3429ce6cdbe5d3b40303a1628fa370b35171
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-08-19 21:29:35 -0400

    Remove stest and expound from namespace

--- src/piggy/compat/alpha.clj
+++ src/piggy/compat/alpha.clj
@@ -1,8 +1,6 @@
 (ns piggy.compat.alpha
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as sgen]
-            [clojure.spec.test.alpha :as stest]
-            [expound.alpha :as expound]))
+            [clojure.spec.gen.alpha :as sgen]))
 ;; specs
 
 (s/def ::fargs (s/or :symbol qualified-symbol?
