151827a4ca5bca427bd3095337b818e6a7ccb211
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-11-01 17:16:51 -0400

    Rename parameters to old and new and add keyword arguments
    Also inline the map for conform*

--- src/piggy/combinators/alpha.clj
+++ src/piggy/combinators/alpha.clj
@@ -2,38 +2,33 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as sgen]))
 
-(defn- ->compat
-  "Make a compat map."
-  [f old new]
-  {:old (f old), :new (f new)})
-
 (defn compat-impl
   "TODO A spec wrapper."
-  [left right]
+  [old new]
   (reify
     s/Specize
     (specize* [s] s)
     (specize* [s _] s)
     s/Spec
-    (conform* [_ x] (->compat #(s/conform* % x) left right))
+    (conform* [_ x] {:old (s/conform old x) :new (s/conform new x)})
     ;; TODO how should it unform? Like s/cat? Cant's specize nil
     ;; Doesn't work with `::s/invalid`
     (unform* [_ x]
-      (or (s/unform* left (first x))
-          (s/unform* right (second x))))
+      (or (s/unform* old (first x))
+          (s/unform* new (second x))))
     (explain* [_ path via in x]
       ;; TODO Still need to figure out explain semantics
-      (apply merge (mapv #(s/explain* % path via in x) [left right])))
+      (apply merge (mapv #(s/explain* % path via in x) [old new])))
     ;; TODO how should overrides work?
     (gen* [_ overrides path rmap]
       (let [gen* (fn [x] (s/gen* x overrides path rmap))]
-        (sgen/frequency [[1 (gen* left)] [1 (gen* right)]])))
+        (sgen/frequency [[1 (gen* old)] [1 (gen* new)]])))
     ;; TODO how should overrides work?
-    (with-gen* [_ gfn] (mapv #(s/with-gen* % gfn) [left right]))
-    (describe* [_] `(compat ~(s/form left) ~(s/form right)))))
+    (with-gen* [_ gfn] (mapv #(s/with-gen* % gfn) [old new]))
+    (describe* [_] `(compat :old ~(s/form old) :new ~(s/form new)))))
 
 (defmacro compat
   "TODO"
-  [left right]
-  `(piggy.compat.alpha/compat-impl (s/spec ~left) (s/spec ~right)))
+  [& {:keys [old new]}]
+  `(compat-impl (s/spec ~old) (s/spec ~new)))
 
