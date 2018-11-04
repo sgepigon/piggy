234ad797c00a9b5a346b37c3e96f7945f521e9f9
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-11-03 22:55:55 -0400

    Support ILookup
    Can call :old or :new on a compat form now e.g.
    (:old (compat :old int? :new number?) returns the :old spec int?.
    
    Also add space between for readability.

--- src/piggy/combinators/alpha.clj
+++ src/piggy/combinators/alpha.clj
@@ -5,27 +5,34 @@
 (defn compat-impl
   "TODO Do not call this directly, use `compat`."
   [old new]
-  (reify
-    s/Specize
-    (specize* [s] s)
-    (specize* [s _] s)
-    s/Spec
-    (conform* [_ x] {:old (s/conform old x) :new (s/conform new x)})
-    ;; TODO how should it unform? Like s/cat? Cant's specize nil
-    ;; Doesn't work with `::s/invalid`
-    (unform* [_ x]
-      (or (s/unform* old (first x))
-          (s/unform* new (second x))))
-    (explain* [_ path via in x]
-      ;; TODO Still need to figure out explain semantics
-      (apply merge (mapv #(s/explain* % path via in x) [old new])))
-    ;; TODO how should overrides work?
-    (gen* [_ overrides path rmap]
-      (let [gen* (fn [x] (s/gen* x overrides path rmap))]
-        (sgen/frequency [[1 (gen* old)] [1 (gen* new)]])))
-    ;; TODO how should overrides work?
-    (with-gen* [_ gfn] (mapv #(s/with-gen* % gfn) [old new]))
-    (describe* [_] `(compat :old ~(s/form old) :new ~(s/form new)))))
+  (let [specs {:old old :new new}]
+    (reify
+      clojure.lang.ILookup
+      (valAt [this k] (get specs k))
+      (valAt [_ k not-found] (get specs k not-found))
+
+      s/Specize
+      (specize* [s] s)
+      (specize* [s _] s)
+
+      s/Spec
+      (conform* [_ x] {:old (s/conform old x) :new (s/conform new x)})
+      ;; TODO how should it unform? Like s/cat? Cant's specize nil
+      ;; Doesn't work with `::s/invalid`
+      (unform* [_ x]
+        (or (s/unform* old (first x))
+            (s/unform* new (second x))))
+      (explain* [_ path via in x]
+        ;; TODO Still need to figure out explain semantics
+        (apply merge (mapv #(s/explain* % path via in x) [old new])))
+      ;; TODO how should overrides work?
+      (gen* [_ overrides path rmap]
+        (let [gen* (fn [x] (s/gen* x overrides path rmap))]
+          (sgen/frequency [[1 (gen* old)] [1 (gen* new)]])))
+      ;; TODO how should overrides work?
+      (with-gen* [_ gfn] (mapv #(s/with-gen* % gfn) [old new]))
+      (describe* [_] `(compat :old ~(s/form old) :new ~(s/form new))))))
+
 
 (s/fdef compat
   :args (s/cat :kwargs (s/keys* :req-un [::old ::new])))
