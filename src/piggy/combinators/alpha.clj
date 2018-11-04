d59c5e01766b51821635be26f25c10d592d9206a
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-11-04 18:18:32 -0500

    Simplify argspec
    I don't need to wrap s/keys* in an s/cat because s/keys* is already makes an
    regex-spec.

--- src/piggy/combinators/alpha.clj
+++ src/piggy/combinators/alpha.clj
@@ -33,9 +33,8 @@
       (with-gen* [_ gfn] (compat-impl old new gfn))
       (describe* [_] `(compat :old ~(s/form old) :new ~(s/form new))))))
 
-
 (s/fdef compat
-  :args (s/cat :kwargs (s/keys* :req-un [::old ::new] :opt-un [::gen])))
+  :args (s/keys* :req-un [::old ::new] :opt-un [::gen]))
 (defmacro compat
   "TODO"
   [& {:keys [old new gen]}]
