79a2b204096594e31611f4068ca8f3e8645eae0b
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-11-05 23:05:23 -0500

    [WIP] Use compat function and macro as template for fcompat

--- src/piggy/combinators/alpha.clj
+++ src/piggy/combinators/alpha.clj
@@ -44,3 +44,41 @@
   [& {:keys [old new gen]}]
   `(compat-impl (s/spec ~old) (s/spec ~new) ~gen))
 
+(defn fcompat-impl
+  "TODO"
+  [old new gfn]
+  (let [specs {:old old :new new}]
+    (reify
+      clojure.lang.ILookup
+      (valAt [_ k] (get specs k))
+      (valAt [_ k not-found] (get specs k not-found))
+
+      s/Specize
+      (specize* [s] s)
+      (specize* [s _] s)
+
+      s/Spec
+      (conform* [_ x] {:old (s/conform old x) :new (s/conform new x)})
+      (unform* [_ x]
+        (if (s/invalid? (:old x))
+          (s/unform* new (:new x))
+          (s/unform* old (:old x))))
+      (explain* [_ path via in x]
+        (let [old-prob (s/explain* old (conj path :old) via in x)
+              new-prob (s/explain* new (conj path :new) via in x)]
+          (into old-prob new-prob)))
+      (gen* [_ overrides path rmap]
+        (if gfn
+          (gfn)
+          (sgen/frequency [[1 (s/gen* old overrides path rmap)]
+                           [1 (s/gen* new overrides path rmap)]])))
+      (with-gen* [_ gfn] (compat-impl old new gfn))
+      (describe* [_] `(fcompat :old ~(s/form old) :new ~(s/form new))))))
+
+(s/fdef fcompat
+  :args (s/keys* :req-un [::old ::new] :opt-un [::gen]))
+(defmacro fcompat
+  "TODO"
+  [& {:keys [old new gen]}]
+  `(fcompat-impl (s/spec ~old) (s/spec ~new) ~gen))
+
