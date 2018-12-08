9aa65039fcb2e8bf46e3726fd53a994c55c58296
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-12-07 22:41:35 -0500

    Use #' reader macro and remove copied spec private helper functions
    Refer directly to the function’s var with #' reader macro. See
    http://blog.danieljanus.pl/blog/2012/04/25/how-to-call-a-private-function-in-clojure/,
    http://blog.fogus.me/2010/09/03/monkeying-with-clojures-deftest/, and
    https://stackoverflow.com/questions/37471253/.

--- src/piggy/combinators/alpha.clj
+++ src/piggy/combinators/alpha.clj
@@ -105,20 +105,6 @@
     [false true] (compat :new (k new) :frequency frequency)
     [false false] nil))
 
-(defn- call-valid?
-  "Returns true if `f` and `args` are valid for `specs`, otherwise returns falsey.
-
-  See `s/call-valid?`."
-  [f specs args]
-  (let [cargs (s/conform (:args specs) args)]
-    (when-not (s/invalid? cargs)
-      (let [ret (apply f args)
-            cret (s/conform (:ret specs) ret)]
-        (and (not (s/invalid? cret))
-             (if (:fn specs)
-               (s/valid? (:fn specs) {:args cargs :ret cret})
-               true))))))
-
 (defn- validate-fn
   "Returns `f` if valid for `specs`, else the smallest shrunk.
 
@@ -127,44 +113,11 @@
   ;; Use `s/gen*` because `s/gen` suppresses invalid generated values. To test
   ;; the args spec, we need to pass potentially invalid values.
   (let [g (s/gen* (:args specs) nil [] {::s/recursion-limit s/*recursion-limit*})
-        prop (sgen/for-all* [g] #(call-valid? f specs %))]
+        prop (sgen/for-all* [g] #(#'s/call-valid? f specs %))]
     (let [ret (sgen/quick-check iters prop)]
       (if-let [[smallest] (-> ret :shrunk :smallest)]
         smallest
         f))))
-
-(defn- spec-name
-  "If `spec` has a name, return it, otherwise nil.
-
-  See `s/spec-name`."
-  [spec]
-  (cond
-    (ident? spec) spec
-    (s/regex? spec) (::s/name spec)
-    (instance? clojure.lang.IObj spec) (::s/name (meta spec))))
-
-(defn- deep-resolve
-  "Resolve `sym-or-k` to a spec/regex.
-
-  See `s/deep-resolve`."
-  [sym-or-k]
-  (if (ident? sym-or-k)
-    (recur (s/get-spec sym-or-k))
-    sym-or-k))
-
-(defn- explain
-  "Return a collection of `s/explain` problems, which is a map with at least keys
-  `:path`, `:pred`, `:val`, `:via`, and `:in`.
-
-  See `s/explain-1`."
-  ([spec path via in v]
-   (explain nil spec path via in v))
-  ([form pred path via in v]
-   ;; try to resolve `pred` to a spec, otherwise return nil. See `s/maybe-spec`.
-   (let [pred (or (deep-resolve pred) (s/spec? pred) (s/regex? pred) nil)]
-     (if (s/spec? pred)
-       (s/explain* pred path (if-let [name (spec-name pred)] (conj via name) via) in v)
-       [{:path path :pred form :val v :via via :in in}]))))
 
 (defn fcompat-impl
   "Do not call this directly, use `fcompat`."
@@ -199,17 +152,17 @@
             (when-not (identical? f args)
               (let [cargs (s/conform (:args specs) args)]
                 (if (s/invalid? cargs)
-                  (explain (:args specs) (conj path :args) via in args)
+                  (#'s/explain-1 nil (:args specs) (conj path :args) via in args)
                   (let [ret (try (apply f args) (catch Throwable t t))]
                     (if (instance? Throwable ret)
                       ;; When `s/fspec` adds exception data, add exception data.
                       [{:path path :pred '(apply fn) :val args :via via :in in :reason (.getMessage ^Throwable ret)}]
                       (let [cret (s/conform (:ret specs) ret)]
                         (if (s/invalid? cret)
-                          (explain (:ret specs) (conj path :ret) via in ret)
+                          (#'s/explain-1 nil (:ret specs) (conj path :ret) via in ret)
                           (when (:fn specs)
-                            (explain (:fn specs) (conj path :fn) via in {:args cargs :ret cret}))))))))))
-          (explain 'ifn? ifn? path via in f)))
+                            (#'s/explain-1 nil (:fn specs) (conj path :fn) via in {:args cargs :ret cret}))))))))))
+          (#'s/explain-1 'ifn? ifn? path via in f)))
       (gen* [_ overrides _ _]
         (if gfn
           (gfn)
